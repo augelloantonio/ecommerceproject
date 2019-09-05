8ec503bbd36884e479a9b539867c086747b75b14
gello94 <gello94@live.it>
2019-09-05 21:59:43 +0100

    spaced

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -70,11 +70,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
 if "DATABASE_URL" in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 else:
     print("Database URL not found. Using SQLite instead")
     DATABASES = {
